cc5c0c32a247bc7d0504752ea595666d018afe34
Sergio Granada <chechogra@gmail.com>
2016-05-28 12:06:39 -0500

    project structure changes first part

--- gulp/styles.js
+++ gulp/styles.js
@@ -26,8 +26,8 @@ var buildStyles = function() {
   };
 
   var injectFiles = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.scss'),
-    path.join('!' + conf.paths.src, '/app/index.scss')
+    path.join(conf.paths.src, '/app/styles/**/*.scss'),
+    path.join('!' + conf.paths.src, '/app/styles/main.scss')
   ], { read: false });
 
   var injectOptions = {
@@ -42,7 +42,7 @@ var buildStyles = function() {
 
 
   return gulp.src([
-    path.join(conf.paths.src, '/app/index.scss')
+    path.join(conf.paths.src, '/app/styles/main.scss')
   ])
     .pipe($.inject(injectFiles, injectOptions))
     .pipe(wiredep(_.extend({}, conf.wiredep)))
