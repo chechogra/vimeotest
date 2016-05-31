672e208c67300b43b8e001fd8699f500012fc530
Sergio Granada <chechogra@gmail.com>
2016-05-30 19:20:41 -0500

    vimeo api config setup

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -3,6 +3,10 @@
 
   angular
     .module('vimeotest')
-    .constant('accessToken', '1b6ac9d70dc6d8d6ff5b5c47b99fb371');
+    .constant('vimeoConfig', {
+      'API_HOST': 'https://api.vimeo.com/',
+      'ACCESS_TOKEN': '1b6ac9d70dc6d8d6ff5b5c47b99fb371',
+      'CATEGORIES_URL': 'categories'
+    });
 
 })();
